42951d2eab7bb8788844093991d73ea4cc496f34
OFFMIND <92588712+0FFMIND@users.noreply.github.com>
2024-04-06 13:14:03 +0800

    up

--- Assets/ExternalTools/TransitionBlocks/Scripts/Transitioner.cs
+++ Assets/ExternalTools/TransitionBlocks/Scripts/Transitioner.cs
@@ -107,9 +107,7 @@ public class Transitioner : MonoBehaviour
     }
 
     #endregion
-
-
-    #region Load scene using string
+    
     public void TransitionToScene(string sceneName, bool waitUntilReady = false)
     {
         if (_canTransition)
@@ -127,10 +125,7 @@ public class Transitioner : MonoBehaviour
         yield return StartCoroutine(Transition(TransitionType.Out));
         SceneManager.LoadScene(sceneName);
     }
-    #endregion
 
-
-    #region Load Scene using index
     public void TransitionToScene(int sceneNumber, bool waitUntilReady = false)
     {
         if (_canTransition)
@@ -148,10 +143,7 @@ public class Transitioner : MonoBehaviour
         yield return StartCoroutine(Transition(TransitionType.Out));
         SceneManager.LoadScene(sceneNumber);
     }
-    #endregion
 
-
-    #region YourGameLogic
     public void TransitionOutWithoutChangingScene()
     {
         if (_canTransition)
@@ -165,13 +157,6 @@ public class Transitioner : MonoBehaviour
         Destroy(_transitionOrdererObject);
         StartCoroutine(ActualTransitionIn());
     }
-
-    public void FinishTransition()
-    {
-        _transitionInTriggered = true;
-    }
-    #endregion
-
 
     private void OnEnable()
     {
@@ -192,6 +177,10 @@ public class Transitioner : MonoBehaviour
         }
     }
 
+    public void FinishTransition()
+    {
+        _transitionInTriggered = true;
+    }
 
     private IEnumerator ActualTransitionIn()
     {
